415ac64e80f31d7041528a43bc57254b7524828a
Mihail Ivanov <tranquilized.mind@gmail.com>
2015-06-27 17:22:53 +0500

    Controller method for solving

---
+++ src/MathMate/MathMate.Web/Controllers/LinearController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using MathMate.Linear;
+
+namespace MathMate.Web.Controllers
+{
+    public class LinearController : Controller
+    {
+        private readonly ISolver solver;
+
+        public LinearController(ISolver solver)
+        {
+            this.solver = solver;
+        }
+
+        public ActionResult Solve(string[] equations)
+        {
+            //var result = solver.Solve(null);
+
+            return Json(equations);
+        }
+    }
+}
--- src/MathMate/MathMate.Web/Scripts/app/controllers/linearController.js
+++ src/MathMate/MathMate.Web/Scripts/app/controllers/linearController.js
@@ -1,24 +1,37 @@
 ﻿(function () {
-    function linearController($scope) {
+    function linearController($scope, $http) {
         var vm = this;
         vm.linearSystem = "";
         vm.display = null;
+        vm.solution = null;
 
         function formatInput() {
             var start = '\\begin{cases}';
             var end = '\\end{cases}';
-            return start + vm.linearSystem + end;
+            var lines = vm.linearSystem.split(/\r|\r\n|\n/).join('\\\\');
+            return start + lines + end;
         }
 
         MathJax.Hub.Queue(function () {
             vm.display = MathJax.Hub.getAllJax("linearSystem")[0];
+            vm.solution = MathJax.Hub.getAllJax("solution")[0];
         });
         
         vm.updateSystem = function () {
             var input = formatInput();
             MathJax.Hub.Queue(["Text", vm.display, input]);
         };
+
+        vm.solve = function() {
+            $http.post('/Linear/Solve', { equations: vm.linearSystem.split(/\r|\r\n|\n/) })
+                 .success(function(data) {
+                    alert(data);
+                  })
+                 .error(function (data) {
+                    alert(data);
+                  });
+        }
     }
     angular.module('mathMate-app')
-        .controller("LinearController", ['$scope', linearController]);
+        .controller("LinearController", ['$scope', '$http', linearController]);
 })();
