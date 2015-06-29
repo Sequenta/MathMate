3d06e16b1917e3824d1b1a53210c59320eb4d516
Mihail Ivanov <tranquilized.mind@gmail.com>
2015-06-29 14:35:20 +0500

    Simplify equations before solving the system

--- src/MathMate/MathMate.Linear/EquationsSystem.cs
+++ src/MathMate/MathMate.Linear/EquationsSystem.cs
@@ -7,17 +7,17 @@ namespace MathMate.Linear
 {
     public class EquationsSystem
     {
-        private readonly IEnumerable<Equation> equations;
+        public IEnumerable<Equation> Equations { get; private set; }
 
         public EquationsSystem(IEnumerable<Equation> equations)
         {
-            this.equations = equations;
+            Equations = equations;
         }
 
         public DenseMatrix GetMatrix()
         {
             var rows = new List<double[]>();
-            foreach (var equation in equations)
+            foreach (var equation in Equations)
             {
                 rows.Add(equation.EquationPairs.Select(x => x.Constant).ToArray());
             }
@@ -29,7 +29,7 @@ namespace MathMate.Linear
 
         public Vector<double> GetFreeTermsVector()
         {
-            var constants = equations.Select(x => x.Result.Constant).ToArray();
+            var constants = Equations.Select(x => x.Result.Constant).ToArray();
             var freeTerms = Vector.Build.Dense(constants);
             return freeTerms;
         }
--- src/MathMate/MathMate.Linear/Solver.cs
+++ src/MathMate/MathMate.Linear/Solver.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using MathMate.Linear.Extensions;
 using MathNet.Numerics.LinearAlgebra;
 
@@ -9,13 +11,20 @@ namespace MathMate.Linear
 
         public Vector<double> Solve(EquationsSystem system)
         {
-            var systemMatrix = system.GetMatrix();
-            var freeTerms = system.GetFreeTermsVector();
+            var simplifiedSystem = Simplify(system);
+            var systemMatrix = simplifiedSystem.GetMatrix();
+            var freeTerms = simplifiedSystem.GetFreeTermsVector();
             if (systemMatrix.IsSquare())
             {
                 strategy = new MatrixMethod();
             }
             return strategy.Solve(systemMatrix,freeTerms);
+        }
+
+        private EquationsSystem Simplify(EquationsSystem system)
+        {
+            var simplifiedEquations = system.Equations.Select(x => x.Simplify());
+            return new EquationsSystem(simplifiedEquations);
         }
     }
 }
