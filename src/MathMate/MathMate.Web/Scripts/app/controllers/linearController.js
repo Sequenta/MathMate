2e203c33fcd963938c194e3bb2ae144047e1077d
Mihail Ivanov <tranquilized.mind@gmail.com>
2015-06-29 11:35:03 +0500

    Dispaly solution

--- src/MathMate/MathMate.Web/Scripts/app/controllers/linearController.js
+++ src/MathMate/MathMate.Web/Scripts/app/controllers/linearController.js
@@ -5,10 +5,8 @@
         vm.display = null;
         vm.solution = null;
 
-        function formatInput() {
-            var start = '\\begin{cases}';
-            var end = '\\end{cases}';
-            var lines = vm.linearSystem.split(/\r|\r\n|\n/).join('\\\\');
+        function formatInput(start,lines,end) {
+            lines = lines.join('\\\\');
             return start + lines + end;
         }
 
@@ -18,15 +16,16 @@
         });
         
         vm.updateSystem = function () {
-            var input = formatInput();
+            var input = formatInput('\\begin{cases}', vm.linearSystem.split(/\r|\r\n|\n/), '\\end{cases}');
             MathJax.Hub.Queue(["Text", vm.display, input]);
         };
 
         vm.solve = function() {
             $http.post('/Linear/Solve', { equations: vm.linearSystem.split(/\r|\r\n|\n/) })
                  .success(function(data) {
-                    alert(data);
-                  })
+                    var result = formatInput('\\begin{pmatrix}', data.Data , '\\end{pmatrix}');
+                    MathJax.Hub.Queue(["Text", vm.solution, result]);
+                })
                  .error(function (data) {
                     alert(data);
                   });
